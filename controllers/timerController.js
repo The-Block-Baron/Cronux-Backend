84c1e19a6fb7f394a6f56173408c47b1b61c9b4c
Diego Prieto <prieto.diego@proton.me>
2023-08-15 17:52:39 +0200

    controlador para pausar crono de la tarea

--- controllers/timerController.js
+++ controllers/timerController.js
@@ -51,3 +51,37 @@ export const startProjectTimer = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const pauseTaskTimer = async (req, res) => {
+    const { projectId, taskId } = req.params;
+
+    try {
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+
+        const task = project.tasks.id(taskId);
+
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        if (task.timerRunning) {
+            const currentTime = new Date();
+            const elapsedMillisecs = currentTime - task.timerStartedAt;
+            task.timeSpent += elapsedMillisecs;
+            task.timerRunning = false;
+            task.timerStartedAt = null;
+
+            project.timeInput += elapsedMillisecs;  // Suma el tiempo a la cuenta del proyecto
+
+            await project.save();
+        }
+
+        res.status(200).json({ message: 'Timer paused for the task.' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
