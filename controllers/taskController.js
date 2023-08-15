d59788191df15bea3d85e8d654f1043b87fe6333
Diego Prieto <prieto.diego@proton.me>
2023-08-15 17:41:15 +0200

    controlador para actualizar la tarea

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -21,3 +21,49 @@ export const createTask = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const readTasks = async (req, res) => {
+    const { projectId } = req.params;
+
+    try {
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+
+        res.status(200).json({ tasks: project.tasks });
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const updateTask = async (req, res) => {
+    const { projectId, taskId } = req.params;
+    const { description, status, value } = req.body;  // Y cualquier otro campo que desees actualizar
+
+    try {
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+
+        const task = project.tasks.id(taskId);
+
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        // Actualiza los valores aquí
+        if (description) task.description = description;
+        if (status) task.status = status;
+        if (value) task.value = value;
+
+        await project.save();
+
+        res.status(200).json({ message: 'Task updated successfully', task });
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+}
