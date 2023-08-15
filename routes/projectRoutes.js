28769c63463ba8707442761978966e90a77ad758
Diego Prieto <prieto.diego@proton.me>
2023-08-15 18:19:23 +0200

    rutas para los  cronos de los proyectos hechas

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import { createProject, readProject, updateProject, deleteProject } from '../controllers/projectController.js'
 import authMiddleware from '../services/authMiddleware.js'
+import { startProjectTimer, pauseProjectTimer } from '../controllers/timerController.js'
 
 const router = express.Router()
 
 router.post('/', authMiddleware, createProject)
+
 router.get('/', authMiddleware, readProject)
+
 router.put('/:projectId', authMiddleware, updateProject)
+
 router.delete('/:projectId', authMiddleware, deleteProject)
+
+router.post('/projects/:projectId/start', startProjectTimer);
+
+router.post('/projects/:projectId/pause', pauseProjectTimer);
 
 export default router 
