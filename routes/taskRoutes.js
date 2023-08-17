977f8631388c0708fcf822730c85e6966db1b2e8
Diego Prieto <prieto.diego@proton.me>
2023-08-17 11:45:20 +0200

    authMiddleware añadido a las rutas de tareas

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,19 +4,20 @@ import {
 } from '../controllers/taskController.js'
 import { startTaskTimer } from '../controllers/timerStartController.js'
 import { pauseTaskTimer } from '../controllers/timerPauseController.js'
+import authMiddleware from '../services/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/:projectId/tasks', createTask)
+router.post('/:projectId/tasks', authMiddleware, createTask)
 
-router.put('/:projectId/tasks/:taskId', updateTask)
+router.put('/:projectId/tasks/:taskId', authMiddleware, updateTask)
 
-router.get('/:projectId/tasks', readTasks)
+router.get('/:projectId/tasks', authMiddleware, readTasks)
 
-router.delete('/:projectId/tasks/:taskId', deleteTask)
+router.delete('/:projectId/tasks/:taskId', authMiddleware, deleteTask)
 
-router.post('/:projectId/tasks/:taskId/start', startTaskTimer);
+router.post('/:projectId/tasks/:taskId/start', authMiddleware, startTaskTimer);
 
-router.post('/:projectId/tasks/:taskId/pause', pauseTaskTimer);
+router.post('/:projectId/tasks/:taskId/pause', authMiddleware, pauseTaskTimer);
 
 export default router
