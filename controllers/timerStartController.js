daa2ed1a4b1596c8202abb20c55f1dac0c2498fd
Diego Prieto <prieto.diego@proton.me>
2023-08-17 10:15:16 +0200

    startTaskTimer ahora es capaz de detectar más errores. Asegurar que los cronometros de las tareas y los proyectos no se pisen entre sí

--- controllers/timerStartController.js
+++ controllers/timerStartController.js
@@ -11,6 +11,10 @@ export const startTaskTimer = async (req, res) => {
             return res.status(404).json({ message: 'Project not found' });
         }
 
+        if (project.timerRunning) {
+            return res.status(400).json({ message: 'Project timer is already running. Cannot start task timer.' });
+        }
+
         const task = project.tasks.id(taskId);
 
         if (!task) {
@@ -21,14 +25,21 @@ export const startTaskTimer = async (req, res) => {
             return res.status(400).json({ message: 'Cannot start timer for a task marked as done.' });
         }
 
+        const otherRunningTask = project.tasks.find(t => t.timerRunning && t._id.toString() !== taskId);
+        if (otherRunningTask) {
+            return res.status(400).json({ message: 'Another task is already running. Cannot start this task timer.' });
+        }
+
         task.timerRunning = true;
         task.timerStartedAt = new Date();
         task.status = "doing";
+        
+        project.timerRunning = true;
+        project.timerStartedAt = new Date();
 
         const today = new Date().toISOString().split('T')[0];
         let timeEntry = project.timeEntries.find(entry => entry.date.toISOString().split('T')[0] === today);
         if (!timeEntry) {
-
             timeEntry = { date: new Date(), milliseconds: 0 };
             project.timeEntries.push(timeEntry);
         }
@@ -39,6 +50,7 @@ export const startTaskTimer = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
 
 export const startProjectTimer = async (req, res) => {
     const { projectId } = req.params;
