a5c0695f36e8786fde19e77fab8fe74b4f63defb
Diego Prieto <prieto.diego@proton.me>
2023-08-18 11:19:55 +0200

    Ahora los controladores del proyecto gestionan la prioridad y los tags

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -17,35 +17,38 @@ export const readProject = async (req, res) => {
 }
 
 export const createProject = async (req, res) => {
-    const {name} = req.body;
+    const { name, tags, priority } = req.body;
 
-    if(!name ) {
-        return res.status(400).json({message: "Name is required"});
+    const validPriorities = ['Immediate', 'Ongoing', 'Future'];
+    if (priority && !validPriorities.includes(priority)) {
+        return res.status(400).json({ message: 'Invalid priority value' });
     }
 
     try {
-        const project = new Project({ name, trackingMethod: 'timeInput' });
-        await project.save()
-        const userId = req.user.id;
-        const user = await User.findById(userId);
+        const newProject = new Project({
+            name,
+            tags,
+            priority
+        });
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+        await newProject.save();
 
-        user.projects.push(project._id);
-        await user.save();
-
-        res.status(201).json({ message: 'Project created successfully', project });
-    } catch(error) {
+        res.status(201).json({ message: 'Project created successfully', project: newProject });
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
 
+
 export const updateProject = async (req, res) => {
     const { projectId } = req.params;
-    const { name, value, totalValue, status } = req.body;
+    const { name, trackingMethod, value, totalValue, tags, priority, status } = req.body;
+
+    const validPriorities = ['Immediate', 'Ongoing', 'Future'];
+    if (priority && !validPriorities.includes(priority)) {
+        return res.status(400).json({ message: 'Invalid priority value' });
+    }
 
     try {
         const project = await Project.findById(projectId);
@@ -57,19 +60,22 @@ export const updateProject = async (req, res) => {
         if (value && !/^(\d{2}):(\d{2}):(\d{2})$/.test(value)) {
             return res.status(400).json({ message: 'Value must be in HH:MM:SS format for timeInput' });
         }
-        
+
         if (totalValue && !/^(\d{2}):(\d{2}):(\d{2})$/.test(totalValue)) {
             return res.status(400).json({ message: 'TotalValue must be in HH:MM:SS format for timeInput' });
         }
 
         if (status && !['doing', 'done', 'to do'].includes(status)) {
-            return res.status(400).json({ message: 'Invalid status provided' });
+            return res.status(400).json({ message: 'Invalid status value' });
         }
 
         if (name) project.name = name;
+        if (trackingMethod) project.trackingMethod = trackingMethod;
         if (value) project.value = value;
         if (totalValue) project.totalValue = totalValue;
         if (status) project.status = status;
+        if (tags) project.tags = tags;
+        if (priority) project.priority = priority;
 
         await project.save();
 
@@ -77,7 +83,8 @@ export const updateProject = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-};
+}
+
 
 
 
